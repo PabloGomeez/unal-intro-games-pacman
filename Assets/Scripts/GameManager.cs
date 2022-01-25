adb734e879fc983fd20c309a2383a71e2d75ea2b
Elizab-hash <65733790+Elizab-hash@users.noreply.github.com>
2022-01-25 11:35:04 -0500

    Added Pellet and PowerPellet

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,6 +7,8 @@ public class GameManager : MonoBehaviour
     public Ghost[] ghosts;
     public Pacman pacman;
     public Transform pellets;
+
+    public int ghostMultiplier { get; private set; } = 1;
     public int score { get; private set; }
     public int lives { get; private set; }
 
@@ -41,6 +43,8 @@ public class GameManager : MonoBehaviour
 
     private void ResetState()
     {
+         ResetGhostMultiplier();
+
          for (int i = 0; i < this.ghosts.Length; i++){
             this.ghosts[i].gameObject.SetActive(true);
         }
@@ -73,7 +77,9 @@ public class GameManager : MonoBehaviour
     public void GhostEaten(Ghost ghost)
 
     {
-        SetScore(this.score + ghost.points);
+        int points = ghost.points * this.ghostMultiplier;
+        SetScore(this.score + points);
+        this.ghostMultiplier++;
     }
 
     public void PacmanEaten()
@@ -90,4 +96,48 @@ public class GameManager : MonoBehaviour
             GameOver();
         }
     }
+
+    public void PelletEaten(Pellet pellet)
+    {
+        pellet.gameObject.SetActive(false);
+
+        SetScore(this.score + pellet.points);
+
+        if (!HasRemainingPellets())
+        {
+            this.pacman.gameObject.SetActive(false);
+            Invoke(nameof(NewRound), 3.0f);
+        }
+    }
+
+    public void PowerPelletEaten(PowerPellet pellet)
+    {
+         //TODO: Changing ghost state
+        PelletEaten(pellet);
+        CancelInvoke();
+        Invoke(nameof (ResetGhostMultiplier), pellet.duration);
+        
+         
+    }
+
+    private bool HasRemainingPellets()
+    {
+        foreach (Transform pellet in this.pellets)
+        {
+           if  (pellet.gameObject.activeSelf){
+               return true;
+           }
+        }
+
+        return false;
+    }
+
+    private void ResetGhostMultiplier()
+    {
+        this.ghostMultiplier = 1;
+    }
+
 }
+
+
+
